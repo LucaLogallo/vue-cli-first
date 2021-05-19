d9f5d64aac6e10170d8d26f11aa16803ce45da38
LucaLogallo <lucalogallo1998@gmail.com>
2021-05-19 15:21:44 +0200

    fine

---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="quadrati">
+      <div class="quadrato"></div>
+      <div class="quadrato"></div>
+      <div class="quadrato"></div>
+      <img src="../assets/logo.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Jumbotron'
+}
+</script>
+
+<style scoped>
+  .quadrati{
+    display: flex;
+    height: 400px;
+    width: 100%;
+    justify-content: space-around;
+    align-items: center;
+    margin: auto;
+  }
+  .quadrato{
+    height: 100px;
+    width: 100px;
+    background-color: red;
+  }
+</style>
